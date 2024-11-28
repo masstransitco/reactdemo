6cd83e0ab740d1429733491d145c3d8767f5a06e
Neuter <mark@air.city>
2024-11-28 14:06:16 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 // src/App.jsx
 
-import React, { useRef, useCallback, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
 import MapSceneToggle from "./components/Map/MapSceneToggle";
 import "./app.css";
-import debounce from "lodash.debounce"; // Ensure lodash.debounce is installed
 
 function App() {
-  const mapViewRef = useRef(null);
-  const sceneViewRef = useRef(null);
-
-
-  // Cleanup debounced functions on unmount to prevent memory leaks
-  useEffect(() => {
-    return () => {
-      handleMapCameraChange.cancel();
-      handleSceneCameraChange.cancel();
-    };
-  }, [handleMapCameraChange, handleSceneCameraChange]);
-
   return (
     <Router>
       <Routes>
